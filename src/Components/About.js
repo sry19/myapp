31732daa6176f3f9464fc55dc4b44390f7b3cfc5
rebeccasun <sun.ruoy@husky.neu.edu>
2020-07-15 18:43:03 -0700

    5

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+const profilepic = require('../images/me.png');
 
 class About extends Component {
   render() {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
+      
       var bio = this.props.data.bio;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+const movieengine = require("../images/portfolio/movieengine.png");
+const taskmanage = require("../images/portfolio/taskmanage.png");
+const othello = require("../images/portfolio/othello.png");
+const rna = require("../images/portfolio/rna.png");
+const yelpcamp = require("../images/portfolio/yelpcamp.png");
+const issuetracker = require("../images/portfolio/issuetracker.png");
 
 class Portfolio extends Component {
+
   render() {
+    let obj = { "movieengine":movieengine,"taskmanage":taskmanage,"othello":othello,"rna":rna, "yelpcamp":yelpcamp, "issuetracker": issuetracker};
 
     if(this.props.data){
       var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
+        var projectImage = projects.image;
         return <div key={projects.title} className="columns portfolio-item">
            <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
+               <img alt={projects.title} src={obj[projectImage]} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
                  <h5>{projects.title}</h5>
